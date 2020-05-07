33bbe3b5d9bc79cbcd3408b8d7da34307f755549
ranamohid <55300223+ranamohid@users.noreply.github.com>
2020-05-07 11:14:34 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import  {Cards}  from "./components";
+import styles from "./App.module.css";
+import { fetchData } from './api/';
+
+class App extends React.Component{
+    state = {
+        data: {},
+    }
+
+    
+      
+   async componentDidMount  () {
+       const fetchedData = await fetchData();
+    
+      this.setState({ data: fetchedData});
+
+    }
+   
+
+   
+    render() {
+
+        const { data } = this.state;
+        
+        return(
+        <div className={styles.container}>
+            <Cards data={data} />
+        </div>
+        );
+    }
+}
+ 
+export default App;
